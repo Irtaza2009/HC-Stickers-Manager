074ce1f171b238ecdcf7cd4170e3aebc92fcd88e
Irtaza2009 <irtazanaqvi05@gmail.com>
2025-07-21 02:16:20 +0500

    updated db backend

--- stickers-backend/server.js
+++ stickers-backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 connectDB();
